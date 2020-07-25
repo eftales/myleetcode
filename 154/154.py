ea3eeeefea061ece2a9df7b26d514d0b656791a6
eftales <1479803084@qq.com>
2020-07-25 16:48:00 +0800

    update:154

---
+++ 154/154.py
@@ -0,0 +1,15 @@
+class Solution:
+    def minArray(self, numbers) -> int:
+        low, high = 0, len(numbers) - 1
+        while low < high:
+            pivot = low + (high - low) // 2
+            if numbers[pivot] < numbers[high]:
+                high = pivot 
+            elif numbers[pivot] > numbers[high]:
+                low = pivot + 1
+            else:
+                high -= 1
+        return numbers[low]
+
+s = Solution()
+print(s.minArray([2,2,2]))
